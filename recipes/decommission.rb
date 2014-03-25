d1f96e7a097f548635f8bddc74096ccdef868528
Kannan Manickam <kannan@rightscale.com>
2014-03-25 15:58:12 -0700

    Unmount the device before destroy. [skip ci]

--- recipes/decommission.rb
+++ recipes/decommission.rb
@@ -21,24 +21,24 @@ marker "recipe_start_rightscale" do
   template "rightscale_audit_entry.erb"
 end
 
-if node['rs-storage']['device']['destroy_on_decomission'] == true ||
-  node['rs-storage']['device']['destroy_on_decomission'] == 'true'
+if node['rs-storage']['device']['destroy_on_decommission'] == true ||
+  node['rs-storage']['device']['destroy_on_decommission'] == 'true'
   nickname = node['rs-storage']['device']['nickname']
 
-  # Remove any characters other than alphanumeric and dashes and replace with dashes
-  sanitized_nickname = nickname.downcase.gsub(/[^-a-z0-9]/, '-')
-
-  # Construct the logical volume from the name of the volume group and the name of the logical volume similat to how the
-  # lvm cookbook constructs the name during the creation of the logical volume
-  logical_volume_device = "/dev/mapper/#{to_dm_name("#{sanitized_nickname}-vg")}-#{to_dm_name("#{sanitized_nickname}-lv")}"
-
-  log "Unmounting #{node['rs-storage']['device']['mount_point']}"
-  mount node['rs-storage']['device']['mount_point'] do
-    device logical_volume_device
-    action [:umount, :disable]
-  end
-
   if is_lvm_used?(node['rs-storage']['device']['mount_point'])
+    # Remove any characters other than alphanumeric and dashes and replace with dashes
+    sanitized_nickname = nickname.downcase.gsub(/[^-a-z0-9]/, '-')
+
+    # Construct the logical volume from the name of the volume group and the name of the logical volume similat to how the
+    # lvm cookbook constructs the name during the creation of the logical volume
+    logical_volume_device = "/dev/mapper/#{to_dm_name("#{sanitized_nickname}-vg")}-#{to_dm_name("#{sanitized_nickname}-lv")}"
+
+    log "Unmounting #{node['rs-storage']['device']['mount_point']}"
+    mount node['rs-storage']['device']['mount_point'] do
+      device logical_volume_device
+      action [:umount, :disable]
+    end
+
     log "LVM is used on the device(s). Cleaning up the LVM."
     # Clean up the LVM conditionally
     ruby_block 'clean up LVM' do
@@ -53,12 +53,18 @@ if node['rs-storage']['device']['destroy_on_decomission'] == true ||
       end
     end
   else
+    log "Unmounting #{node['rs-storage']['device']['mount_point']}"
+    mount node['rs-storage']['device']['mount_point'] do
+      device lazy { node['rightscale_volume'][nickname]['device'] }
+      action [:umount, :disable]
+    end
+
     log 'LVM was not used on the device, simply detaching the deleting the device.'
     rightscale_volume nickname do
       action [:detach, :delete]
     end
   end
 else
-  log "rs-storage/device/destroy_on_decomission is set to '#{node['rs-storage']['device']['destroy_on_decomission']}'" +
+  log "rs-storage/device/destroy_on_decommission is set to '#{node['rs-storage']['device']['destroy_on_decommission']}'" +
     "  skipping..."
 end
