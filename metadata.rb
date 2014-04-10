1a0d84109174de5e82467871aa7f649054540bab
Kannan Manickam <kannan@rightscale.com>
2014-04-10 10:06:14 -0700

    Minor description change in metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -142,8 +142,8 @@ attribute 'rs-storage/schedule/enable',
 
 attribute 'rs-storage/schedule/hour',
   :display_name => 'Backup Schedule Hour',
-  :description => "The hour to schedule the backup on. Use '*' for taking backups every hour. This value should' +
-    ' abide by crontab crontab syntax. Example: 23",
+  :description => "The hour to schedule the backup on. This value should abide by crontab syntax. Use '*' for taking' +
+    ' backups every hour. Example: 23",
   :recipes => ['rs-storage::schedule'],
   :required => 'required'
 
