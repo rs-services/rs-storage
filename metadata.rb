2b045f4a613b02dc6a0b0c51862a27dfee3206bf
Kannan Manickam <kannan@rightscale.com>
2014-04-10 16:51:51 -0700

    Bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Provides recipes for managing volumes on a Server in a RightScale supported cloud'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'chef_handler'
 depends 'filesystem'
